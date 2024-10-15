bf68b48a8b01ddc938f74fe836b0573620492cca
Rob Winterburn <forestalwinters@gmail.com>
2024-10-15 13:06:33 +0000

    connected css to page

--- beats/beats/settings.py
+++ beats/beats/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'beats.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'static' / 'templates'],
+        'DIRS': [BASE_DIR / 'static' /'templates' ],
         'APP_DIRS': True,  # Allow searching for templates in each app
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
