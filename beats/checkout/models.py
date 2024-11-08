16da5716d14c3e595912c81ea0b6b0a60dd57fbd
Rob Winterburn <uhvgebc6@students.codeinstitute.net>
2024-11-08 14:03:59 +0000

    added order, orderlineitem, updated profile models and order models added update total calculator

--- beats/checkout/models.py
+++ beats/checkout/models.py
@@ -1,5 +1,11 @@
 # checkout/models.py
 from django.db import models
+import uuid
+from django.db.models import Sum
+from django.conf import settings
+
+from instrumentals.models import Beat
+from profiles.models import Profile
 
  # Make sure this is at the top
 
@@ -18,9 +24,39 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
 
+    def _generate_order_number(self):
+
+        return uuid.uuid4().hex.upper()
+
+
+    def update_total(self):
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total_sum']
+        self.grand_total = self.order_total
+        self.save()
+
+
+
+    def save(self, *args, **kwargs):
+
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.order_number
+
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null= False, blank= False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey()
-    quantity
-    lineitem_total
+    product = models.ForeignKey(Beat, null=False,blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False,blank=False,default=0)
+    lineitem_total = models.DecimalField(max_digits=6,decimal_places=2,null=False,blank=False,editable=False)
+
+    def save(self, *args, **kwargs):
+        # Calculate the total price for this line item
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"SKU{self.product.sku} on order {self.order.order_number}"
--- beats/profiles/models.py
+++ beats/profiles/models.py
@@ -2,14 +2,26 @@
 
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    # Additional fields for the profile
-    bio = models.TextField(blank=True, null=True)
-
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address_line_1 = models.CharField(max_length=255, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=255, blank=False, null=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=50)
+ 
     def __str__(self):
-        return f"{self.user.username}'s Profile"
+        return self.user.username
 
 
 # profiles/models.py
