1ad4eb3085c0d722ed82f71ccad487e66d1ac090
Robert Winterburn <forestalwinters@gmail.com>
2024-12-10 15:56:57 +0000

    successfully deployed and fixed admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,7 +12,7 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
     readonly_fields = ('order_number', 'date', 'order_total', 'grand_total', 'stripe_payment_id','stripe_payment_status','stripe_metadata')
     fields = ('order_number', 'date', 'full_name', 'email', 'phone_number',
-              'country', 'postcode', 'town_or_city', 'street_address1',
+              'country', 'postcode', 'city', 'street_address1',
               'street_address2', 'order_total', 'grand_total',)
     list_display = ('order_number', 'date', 'full_name', 'email', 'order_total', 'grand_total', 'country')
     ordering = ('-date',)
