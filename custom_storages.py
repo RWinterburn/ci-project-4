2627231369272fc1a5961ea2f826624112ea9a48
Robert Winterburn <forestalwinters@gmail.com>
2024-12-11 16:04:46 +0000

    debugging

--- custom_storages.py
+++ custom_storages.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from storages.backends.s3boto3 import s3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(s3Boto3Storage):
+
+class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
 
