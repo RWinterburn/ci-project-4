123b2ec49bcfe9f0e6fe2384d172c3be40e12831
Robert Winterburn <forestalwinters@gmail.com>
2024-12-11 11:14:13 +0000

    adding fixtures

--- beats/settings.py
+++ beats/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from decouple import config
 from pathlib import Path
 import os
+import dj_database_url
 
 import os
 from dotenv import load_dotenv
